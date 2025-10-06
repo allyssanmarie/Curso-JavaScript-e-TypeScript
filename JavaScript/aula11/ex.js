03a34a29479a463f7505ffd57cac5084df0b88c4
Allyssan Marie <allyssan19@gmail.com>
2025-10-06 19:12:58 -0300

    window funções

---
+++ JavaScript/aula11/ex.js
@@ -0,0 +1,7 @@
+let num1 = prompt("Digite o primeiro numero:");
+let num2 = prompt("Digite o segundo numero:");
+
+num1 = Number(num1); //converte de string pra number
+num2 = Number(num2);
+
+alert(`O resultado da soma é: ${num1 + num2}`);
---
+++ JavaScript/aula11/script.js
@@ -0,0 +1,9 @@
+alert ("Com a nossa mensagem"); //o alert é uma funçao/método do window - Mostra uma caixa de alerta
+
+confirm('Deseja realmente fechar esta janela?'); //outra função do window - Mostra uma caixa com "OK" e "Cancelar"
+
+prompt('Digite o seu nome:'); //outra funçao - Mostra uma caixa para o usuário digitar algo
+
+let confirma = confirm("Realmente deseja apagar?"); //guardando o valor retornado na variável
+
+let num1 = prompt('Digite um numero:');  //retorna sempre uma string com o prompt
