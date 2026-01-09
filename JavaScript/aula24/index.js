ec0e3c5cb4ff7d0fdb586ffaae6027cb61dc1896
Allyssan Marie <allyssan19@gmail.com>
2026-01-09 15:06:33 -0300

    if, else if e else

---
+++ JavaScript/aula24/index.js
@@ -0,0 +1,32 @@
+//if, else if e else
+/*
+entre 0 - 11 =  Bom dia
+12 - 17 = Boa tarde
+18 - 23 = Boa noite
+*/
+
+//if pode ser usado sozinho
+
+const hora = 20;
+
+if (hora >= 0 && hora <= 11) {
+    console.log('Bom dia');
+}
+else if (hora >= 12 && hora <= 17) {
+    console.log('Boa tarde');
+}
+else if (hora >= 18 && hora <= 23){
+    console.log('Boa noite');
+}
+else {
+    console.log('Olá')
+}
+
+const tenhoGRana = true;
+
+if (tenhoGRana) {
+    console.log('Vou sair de casa');
+}
+else {
+    console.log('Não vou sair de casa');
+}
